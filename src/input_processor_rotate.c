77d02bb685c47f67360bb9d53c0cbf98cbf36a59
kamijodev <f.kamijodev@gmail.com>
2026-04-07 14:56:13 +0900

    debug: Yハンドラが呼ばれるか確認（X=50固定になるか）

--- src/input_processor_rotate.c
+++ src/input_processor_rotate.c
@@ -44,17 +44,10 @@ static int rotate_handle_event(const struct device *dev, struct input_event *eve
     }
 
     if (event->code == config->x_code) {
-        int32_t skew_factor = (int32_t)param1 + data->skew_offset;
-        int32_t numerator = (int32_t)event->value * SCALE - skew_factor * data->last_y;
-        if (config->track_remainders) {
-            numerator += data->remainder;
-        }
-        event->value = numerator / SCALE;
-        if (config->track_remainders) {
-            data->remainder = numerator - event->value * SCALE;
-        }
+        /* DEBUG: output last_y instead of X. If Y handler works, X=25 (50/2 after scaler) */
+        event->value = data->last_y;
     } else if (event->code == config->y_code) {
-        data->last_y = event->value;
+        data->last_y = 50;
     }
 
     return ZMK_INPUT_PROC_CONTINUE;
